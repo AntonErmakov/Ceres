f05dcfd14540770985fccae902e00b4c76e49f90
AntonErmakov <anton.i.ermakov@gmail.com>
2015-09-03 22:14:03 -0400

    added output new file name, and changed folder structure

--- matlab_support_code/FillConfigTemplate.m
+++ matlab_support_code/FillConfigTemplate.m
@@ -1,9 +1,10 @@
-function FillConfigTemplate(config_template_filename,mesh_filename,append)
+function new_name = FillConfigTemplate(config_template_filename,mesh_filename,append)
 
 [path,name,ext] = fileparts(config_template_filename);
 
 in_old = fopen(config_template_filename,'r');
-in_new = fopen([path '/' name '_' append ext],'w');
+new_name = [path '/' name '_' append ext];
+in_new = fopen(new_name,'w');
 
 str = fgetl(in_old);
 fprintf(in_new,'%s',str);
@@ -16,12 +17,15 @@ while (str~=-1)
         fprintf(in_new,[key_mesh ' = "' mesh_filename(4:end) '";\n']);
     elseif strncmpi(str,key_out,numel(key_out))
             fprintf(in_new,[key_out ' = "output/output_' append '/";\n']);
-            mkdir(['../output/output_' append])
-
+            folder_name = ['../output/output_' append];
+            if (exist(folder_name,'dir') == 7)
+                rmdir(folder_name);
+                mkdir(folder_name);
+            else
+                mkdir(folder_name);
+            end
     else
-        fprintf(in_new,'%s\n',str);
-        
-        
+        fprintf(in_new,'%s\n',str);     
     end   
 end
 
